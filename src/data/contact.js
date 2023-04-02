0c4ac4d20b0923a89a3b4c29f569b2a3c5719744
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2023-04-02 13:03:06 +0100

    add delete things

---
+++ src/data/contact.js
@@ -0,0 +1,37 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+// See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
+// to add other icons.
+
+const data = [
+  {
+    link: 'https://github.com/sanjaynagi',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.instagram.com/sanjaycurtisnagi/',
+    label: 'Instagram',
+    icon: faInstagram,
+  },
+  {
+    link: 'https://www.linkedin.com/in/sanjaycnagi',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://twitter.com/sanjay_c_nagi',
+    label: 'Twitter',
+    icon: faTwitter,
+  },
+  {
+    link: 'mailto:sanjay.c.nagi@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
