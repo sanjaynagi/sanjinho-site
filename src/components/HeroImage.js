5f0a8406de2b8d4ba3418526823747e31fabbc6c
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2023-05-24 14:55:05 +0100

    change pic size

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const HeroImage = () => {
   return (
     <Flex position="relative" pb={4} justifyContent="center">
-      <AspectRatio flexShrink={0} ratio={1} w={240} h={170} as="figure">
+      <AspectRatio flexShrink={0} ratio={1} w={220} h={170} as="figure">
         <Box rounded="full" overflow="hidden" borderRadius={20}>
           <Image
             src="/avatar.jpg"
-            width={240}
+            width={220}
             height={170}
             alt="Avatar Image"
           />
