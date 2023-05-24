3410d955c80a27efc4d3a638369b7a7a58cb79a6
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2023-05-24 14:19:56 +0100

    legooo

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -4,12 +4,12 @@ import Image from 'next/image';
 const HeroImage = () => {
   return (
     <Flex position="relative" pb={4} justifyContent="center">
-      <AspectRatio flexShrink={0} ratio={1} w={200} h={140} as="figure">
-        <Box rounded="full" overflow="hidden" borderRadius={10}>
+      <AspectRatio flexShrink={0} ratio={1} w={240} h={170} as="figure">
+        <Box rounded="full" overflow="hidden" borderRadius={20}>
           <Image
-            src="/avatar.png"
-            width={200}
-            height={140}
+            src="/avatar.jpg"
+            width={240}
+            height={170}
             alt="Avatar Image"
           />
         </Box>
