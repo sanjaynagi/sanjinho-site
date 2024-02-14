11c27185ea4dbfe71a6891f64aeb52161eee23af
Sanjay Curtis Nagi <34922269+sanjaynagi@users.noreply.github.com>
2024-02-14 19:57:21 +0000

    Update software.js

--- src/data/software.js
+++ src/data/software.js
@@ -1,6 +1,6 @@
 export const SoftwareList = [
   {
-    id: '5',
+    id: '7',
     title: ' 🦚 AmpSeeker 🦚',
     description: `A scalable, reproducible snakemake pipeline to analyse Illumina amplicon sequencing data. 
                   Builds a webpage to conveniently explore the results`,
@@ -8,7 +8,14 @@ export const SoftwareList = [
     githubIcon: true
   },
   {
-    id: '1',
+    id: '6',
+    title: ' 🌍 The malaria vector selection atlas 🌍',
+    description: `A web resource of selection signals in An. gambiae s.l mosquitoes from phase 3 of the Anopheles 1000 genomes project`,
+    href: 'https://anopheles-genomic-surveillance.github.io/selection-atlas/home-page.html',
+    githubIcon: true
+  },
+  {
+    id: '2',
     title: '🦕 RNA-Seq-Pop 🦕',
     description: `A reproducible snakemake pipeline to analyse RNA-Seq data from any organism. Calls variants 
                   and performs population genomic analyses as well as typical transcriptomic analyses`,
@@ -16,14 +23,14 @@ export const SoftwareList = [
     githubIcon: true
   },
   {
-    id: '2',
+    id: '3',
     title: '🦟 malariagen_data 🦟 ',
     description: `A python package to access and analyse data from the Anopheles 1000 genomes project in the cloud `,
     href: 'https://github.com/malariagen/malariagen-data-python',
     githubIcon: true
   },
   {
-    id: '3',
+    id: '4',
     title: '🌺 AnoExpress 🌺',
     description: `A python package to load and explore data from a meta-analysis of transcriptomic studies into insecticide
                   resistance in Anopheles mosquitoes`,
@@ -31,7 +38,7 @@ export const SoftwareList = [
     githubIcon: true
   },
   {
-    id: '4',
+    id: '5',
     title: '🧬 AnoPrimer 🧬',
     description: `A python package to design primers in Anopheles gambiae and funestus whilst considering genetic variation.`,
     href: 'https://github.com/sanjaynagi/AnoPrimer',
