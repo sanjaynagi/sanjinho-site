cb765c54ad1ed8120c64e874a28aa43989605b7f
Sanjay Curtis Nagi <34922269+sanjaynagi@users.noreply.github.com>
2025-03-25 19:33:23 +0000

    Update software.js

--- src/data/software.js
+++ src/data/software.js
@@ -11,7 +11,7 @@ export const SoftwareList = [
     id: '6',
     title: ' 🌍 The malaria vector selection atlas 🌍',
     description: `A web resource of selection signals in An. gambiae s.l mosquitoes from phase 3 of the Anopheles 1000 genomes project`,
-    href: 'https://anopheles-genomic-surveillance.github.io/selection-atlas/home-page.html',
+    href: 'https://anopheles-genomic-surveillance.github.io/selection-atlas/',
     githubIcon: true
   },
   {
