5a736338e277dc113f159246ec50e2f32812a658
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2025-03-25 19:35:09 +0000

    remove instagram logo

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,4 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
@@ -11,11 +10,6 @@ const data = [
     link: 'https://github.com/sanjaynagi',
     label: 'Github',
     icon: faGithub,
-  },
-  {
-    link: 'https://www.instagram.com/sanjaycurtisnagi/',
-    label: 'Instagram',
-    icon: faInstagram,
   },
   {
     link: 'https://www.linkedin.com/in/sanjaycnagi',
