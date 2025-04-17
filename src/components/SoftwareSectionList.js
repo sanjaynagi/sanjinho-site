ce3af9c37db207257b3f60fdfba9fb2931a11661
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2025-04-17 10:22:06 +0100

    add github

--- src/components/SoftwareSectionList.js
+++ src/components/SoftwareSectionList.js
@@ -1,11 +1,19 @@
-import { VStack, Heading, List, ListItem } from '@chakra-ui/react';
-
+import { VStack, Heading, List, ListItem, Button } from '@chakra-ui/react';
+import Link from 'next/link';
+import { FaGithub } from "react-icons/fa";
 import ProjectInfoCard from './ProjectInfoCard';
 
 const SoftwareSectionList = ({ projects }) => {
   return (
     <VStack w="full" alignItems="center" spacing={4} as="section" mt={16}>
       <Heading size="lg">Open Source Software I&#39;ve developed or contributed to</Heading>
+
+      <VStack spacing={4} alignItems="center" w="full" as="section" pt={2} >
+        <Button leftIcon={<FaGithub/>} colorScheme='teal' size='lg' variant='ghost'> 
+          <Link href="https://github.com/sanjaynagi">My GitHub</Link>
+        </Button>
+      </VStack>      
+
       <List spacing={6}>
         {projects.map(project => (
           <ListItem key={project.href}>
