ad77f47c50ffc3189372feb684519ce2bc0548d1
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2025-06-16 13:48:17 +0100

    fix

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,6 @@
 
 
 import React, { useRef } from "react";
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 import useScript from "../utils/use-script";
 
 const Comments = () => {
@@ -13,27 +9,15 @@ const Comments = () => {
   const status = useScript({
     url: "https://utteranc.es/client.js",
     theme: "github-light",
-<<<<<<< Updated upstream
-    issueTerm: "url",
-    repo: "sanjaynagi/sanjinho-site",
-    ref: comment
-=======
     issueTerm: "pathname",
     repo: "sanjaynagi/sanjinho-site",
     ref: comment,
     label: "💬 comments", // Optional: adds a label to issues
->>>>>>> Stashed changes
   });
 
   return (
     <div className="w-full">
-<<<<<<< Updated upstream
-      {
         <div ref={comment}></div>
-      }
-=======
-        <div ref={comment}></div>
->>>>>>> Stashed changes
     </div>
   );
 };
