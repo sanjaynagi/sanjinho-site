2698b9f5c24f1750f9b665b6b6b044751a207626
Sanjay Nagi <sanjay.nagi@eit.org>
2026-03-27 23:18:41 +0000

    Fix Next.js 16 deprecation warnings: add image qualities config and remove legacyBehavior from Links

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const nextConfig = {
       { hostname: 'pbs.twimg.com' },
       { hostname: '2022.appjs.co' },
     ],
+    qualities: [25, 50, 75, 100],
     deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
